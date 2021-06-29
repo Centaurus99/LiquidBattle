991313528c6576fc47b8256f39bd7ceb813e5bff
Centaurus99 <1102569568@qq.com>
2021-06-30 00:32:37 +0800

    add notes

--- src/ColorSet.h
+++ src/ColorSet.h
@@ -1,7 +1,7 @@
 /*
  * @Author: Tong Haixuan
  * @Date: 2021-06-30 00:02:46
- * @LastEditTime: 2021-06-30 00:15:50
+ * @LastEditTime: 2021-06-30 00:31:47
  * @LastEditors: Tong Haixuan
  * @Description: The list of some comfortable colors
  */
@@ -11,6 +11,7 @@
 
 #include <Box2D/Common/b2Draw.h>
 
+// The list of some comfortable colors
 b2Color ColorSet[] = {
 	{0.8f, 0.5f, 0.8f},
     {0.4f, 0.8f, 0.8f},
